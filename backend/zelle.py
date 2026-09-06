4827eed7c4cdedbc1cdcb02d160bd2c7fe31fcb0
MagnusUnger <262898955+MagnusUnger@users.noreply.github.com>
2026-09-06 19:45:15 +0200

    First Structure

---
+++ backend/zelle.py
@@ -0,0 +1,9 @@
+
+
+class zell_node:
+    def __init__(self, oben=None, unten=None, rechts=None, links=None, temp=20):
+        self.oben = oben
+        self.unten = unten
+        self.rechts = rechts
+        self.links = links
+        self.temp = temp
