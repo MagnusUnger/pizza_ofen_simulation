fc7f5f1db51ccd2397b1644872b36b76736b8445
MagnusUnger <262898955+MagnusUnger@users.noreply.github.com>
2026-09-07 20:14:45 +0200

    Überlegung wegen Numpy Array. zu Bridge

---
+++ bridge/sim_controller.py
@@ -0,0 +1,8 @@
+"""
+Mmh...
+Wenn die Numpy-Array Lösung schneller funktionieren würde wie angenommen.
+    dann könnte ich ja eine Uvicorn FastAPI als Bridge benutzen.
+    Und ein React TypeScript Frontend?
+
+
+"""
